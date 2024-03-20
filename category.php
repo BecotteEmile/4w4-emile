954e78bfb3c56634664ab8c1e5bb0256b5c65ca1
Bécotte <e2256126@cmaisonneuve.qc.ca>
2024-03-20 10:55:26 -0400

    s7c1

--- category.php
+++ category.php
@@ -1,17 +1,8 @@
 <?php get_header(); ?>
-    <h1>FRONT PAGE</h1>
-    <div id="entete" class="global">
-        <section class="entete_header hero">
-            <h1><?= get_bloginfo("name"); ?></h1>
-            <h2><?= get_bloginfo("description"); ?></h2>
-            <h3>Tim - Collège de maisonneuve</h3>
-            <button>Événement</button>
-        </section>
-        <?php get_template_part('gabarits/vague') ?>
-    </div>
+    <h1>CATEGORY</h1>
     <div id="accueil" class="global">
         <section>
-            <h2>Accueil (h2)</h2>
+            <h2>Destinations par categorie</h2>
             <div class="destination">
                 <?php
                     if (have_posts()) :
@@ -32,20 +23,6 @@
                 <?php endif; ?>
             </div>
             
-            
-            <!-- <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ad dolorum, ex officia similique, illum laudantium reiciendis dolor numquam fugiat laboriosam, repudiandae ducimus ipsam inventore adipisci. Optio, veritatis! Atque, culpa.</p> -->
-            <!-- <p class="boite1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ad dolorum, ex officia similique, illum laudantium reiciendis dolor numquam fugiat laboriosam, repudiandae ducimus ipsam inventore adipisci. Optio, veritatis! Atque, culpa.</p>
-            <p class="boite2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ad dolorum, ex officia similique, illum laudantium reiciendis dolor numquam fugiat laboriosam, repudiandae.</p> -->
-        
-        </section>
-    </div>
-    <div id="evenement" class="global diagonale">
-        <section>
-            <h2>Événement (h2-tres grand)</h2>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, earum?(h3-grand)</h3>
-            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, earum?(h4-moyen)</h4>
-            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, earum?(h5-petit)</h5>
-            <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, earum?(h6-tres petit)</h6>
         </section>
     </div>
     <div id="galerie" class="global">
